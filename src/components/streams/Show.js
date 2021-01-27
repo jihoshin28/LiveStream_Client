5da9eebef176c1099428c891b4f37b72c53ae359
Allen <jihoshin.28@gmail.com>
2021-01-26 20:13:50 -0800

    live stream functional

--- src/components/streams/Show.js
+++ src/components/streams/Show.js
@@ -1,23 +1,51 @@
 import React from 'react'
+import flv from 'flv.js'
 import { connect } from 'react-redux'
 import {getStream} from '../../actions'
 
 //const NodeMediaServer = require('node-media-server');
 
 class StreamShow extends React.Component {
-    componentDidMount(){
-        console.log(this.match)
-        this.props.getStream(this.props.match.params.id)
+
+    constructor(props){
+        super(props)
+        this.videoRef = React.createRef()
     }
-    // renderStream(){
 
-    // }
+    componentDidMount(){
+        const {id} = this.props.match.params
+        this.props.getStream(id)
+        this.buildPlayer()
+    }
 
+    componentDidUpdate(){
+        this.buildPlayer()
+    }
+    
+    buildPlayer(){
+        if(this.player || !this.props.stream){
+            return;
+        }
+
+        const {id} = this.props.match.params
+
+        this.player = flv.createPlayer({
+            type: 'flv',
+            url: `http://localhost:8000/live/${id}.flv`
+        })
+
+        this.player.attachMediaElement(this.videoRef.current)
+        this.player.load()
+    }
     
     render(){
+        if(!this.props.stream){
+            return <div>Loading...</div>
+        }
         const {title, description} = this.props.stream
         return(
             <div>
+                <video ref = {this.videoRef} style = {{width: '100%'}} controls = {true}/>
                 <h1>{title}</h1>
                 <h4>{description}</h4>
             </div>
